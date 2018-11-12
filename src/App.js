70554c802e0f217e7392c4ae792336966ffb707a
zhang <zhang@example.com>
2018-11-12 11:11:18 +0800

    代码初始化整合

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         hello word
+      </div>
+    );
+  }
+}
+
+export default App;
